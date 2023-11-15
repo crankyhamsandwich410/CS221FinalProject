8fef9fa215e8056f1981eb9ec2c5c00ff75c9842
Splandex <104538485+Splandex@users.noreply.github.com>
2023-11-15 16:08:36 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,21 @@
 let slideIndex = 1;
 
-const SLIDE_WAIT_IN_SECONDS = 6 
+const SLIDE_WAIT_IN_SECONDS = 1.5 
 
 const images = [
-    "url('https://www.w3schools.com/howto/img_woods.jpg')",
-    "url('https://www.w3schools.com/howto/img_5terre.jpg')",
-    "url('https://www.w3schools.com/howto/img_lights.jpg')",
+    "url('Assets/louis-hansel-tLbGQ1YazV8-unsplash.jpg')",
+    "url('Assets/louis-hansel-VhqBh21uz0k-unsplash.jpg')",
+    "url('Assets/orkun-orcan-epQ32Sf0_XQ-unsplash.jpg')",
+    "url('Assets/paul-griffin-WWST6E8LxeE-unsplash.jpg')",
+    "url('Assets/sebastian-schuppik-H7xTpvBjJS4-unsplash.jpg')",
+    "url('Assets/toa-heftiba-PSzZSuQsoNU-unsplash.jpg')",
+    "url('Assets/urban-gyllstrom-MaWMfm-HCqQ-unsplash.jpg')",
+    "url('Assets/alexandra-tran-fWfRv-qdlUU-unsplash.jpg')",
+    "url('Assets/casey-lee-awj7sRviVXo-unsplash.jpg')",
+    "url('Assets/ivan-stern-LOLSb7m6XkU-unsplash.jpg')",
+    "url('Assets/justus-menke-62XLglIrTJc-unsplash.jpg')",
+    "url('Assets/karol-chomka-YY-phoTAZjw-unsplash.jpg')",
+    "url('Assets/louis-hansel-HF16IxB_tO4-unsplash.jpg')"
 ]
 
 // Wait function that returns a callback
@@ -22,6 +32,7 @@ function showSlides(n) {
     let slide = document.getElementById("slideDiv");
     slide.style.backgroundImage = images[slideIndex];
 
+    console.log(images[slideIndex])
     sleep(SLIDE_WAIT_IN_SECONDS * 1000).then(() => { 
         slideIndex += 1;
         showSlides(slideIndex)
@@ -32,10 +43,7 @@ function showSlides(n) {
 
 
 window.onload=function() {
-    sleep(SLIDE_WAIT_IN_SECONDS * 1000).then(() => { 
-        slideIndex += 1;
-        showSlides(slideIndex)
-    });
+    showSlides(slideIndex)
 }
 
 

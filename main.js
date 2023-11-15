9370039e196e7c1073785ee9b51a0ddbf22e9239
Splandex <104538485+Splandex@users.noreply.github.com>
2023-11-15 15:30:33 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -2,6 +2,12 @@ let slideIndex = 1;
 
 const SLIDE_WAIT_IN_SECONDS = 6 
 
+const images = [
+    "url('https://www.w3schools.com/howto/img_woods.jpg')",
+    "url('https://www.w3schools.com/howto/img_5terre.jpg')",
+    "url('https://www.w3schools.com/howto/img_lights.jpg')",
+]
+
 // Wait function that returns a callback
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
@@ -9,28 +15,27 @@ function sleep(ms) {
 
 // Recursive function that renders the slide at the slide index
 function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("slideImage");
-  if (n > slides.length) {
-    slideIndex = 1
-  }
-  if (n < 1) {
-    slideIndex = slides.length
-  }
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.opacity = 0;
-  }
-  slides[slideIndex-1].style.opacity = 1;
+    let i = images.length;
+    if (slideIndex >= i) {
+        slideIndex = 0
+    }
+    let slide = document.getElementById("slideDiv");
+    slide.style.backgroundImage = images[slideIndex];
 
-  sleep(SLIDE_WAIT_IN_SECONDS * 1000).then(() => { 
-    slideIndex += 1;
-    showSlides(slideIndex)
-  });
+    sleep(SLIDE_WAIT_IN_SECONDS * 1000).then(() => { 
+        slideIndex += 1;
+        showSlides(slideIndex)
+    });
 }
 
 
+
+
 window.onload=function() {
-    showSlides(slideIndex);
+    sleep(SLIDE_WAIT_IN_SECONDS * 1000).then(() => { 
+        slideIndex += 1;
+        showSlides(slideIndex)
+    });
 }
 
 

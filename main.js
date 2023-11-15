416087af6a9314463990652d285c099748878f8b
Splandex <104538485+Splandex@users.noreply.github.com>
2023-11-15 13:46:37 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,72 +1,41 @@
-let list = [
-    "rock",
-    "paper",
-    "scissors"
-]
+let slideIndex = 1;
 
+const SLIDE_WAIT_IN_SECONDS = 6 
 
-function ClickHandler(input) {
-    console.log("Handle")
-    let computerChoice = list[Math.floor(Math.random()*3)]
-    let displayLabel = document.getElementById("result")
-
-    if (list.includes(input) == false) {
-        displayLabel.textContent = "Please choose rock, paper, or scissors"
-        return
-    }
-
-    if (input == computerChoice){
-        displayLabel.textContent = "The computer chose " + computerChoice + ". Tie game"
-    } else if ((input == "rock" && computerChoice == "scissors") 
-    || (input == "paper" && computerChoice == "rock") 
-    || (input == "scissors" && computerChoice == "paper")) 
-    {
-        displayLabel.textContent = "The computer chose " + computerChoice + ". You win"
-    } else {
-        displayLabel.textContent = "The computer chose " + computerChoice + ". You lose"
-    }
+// Wait function that returns a callback
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-function paper() {
-    ClickHandler("paper")
+// Recursive function that renders the slide at the slide index
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {
+    slideIndex = 1
+  }
+  if (n < 1) {
+    slideIndex = slides.length
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.opacity = 0;
+  }
+  slides[slideIndex-1].style.opacity = 1;
+
+  sleep(SLIDE_WAIT_IN_SECONDS * 1000).then(() => { 
+    slideIndex += 1;
+    showSlides(slideIndex)
+  });
 }
-function rock() {
-    ClickHandler("rock")
-}
-function scissors() {
-    ClickHandler("scissors")
-}
+
 
 window.onload=function() {
-    document.getElementById("Paper").addEventListener("click", paper);
-    document.getElementById("Rock").addEventListener("click", rock);
-    document.getElementById("Scissors").addEventListener("click", scissors);
+    showSlides(slideIndex);
 }
 
 
 
 
 
-
-// while (true) {
-//     let input = prompt("Choose rock, paper, or scissors", "Rock").toLowerCase();
-//     let computerChoice = list[Math.floor(Math.random()*3)]
-
-//     if (list.includes(input) == false) {
-//         confirm("Please choose rock, paper, or scissors")
-//         continue
-//     }
-
-//     if (input == computerChoice){
-//         confirm("The computer chose " + computerChoice + ". Tie game")
-//     } else if ((input == "rock" && computerChoice == "scissors") 
-//     || (input == "paper" && computerChoice == "rock") 
-//     || (input == "scissors" && computerChoice == "paper")) 
-//     {
-//         confirm("The computer chose " + computerChoice + ". You win")
-//     } else {
-//         confirm("The computer chose " + computerChoice + ". You lose")
-//     }
-// }
 
  
